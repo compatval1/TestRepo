8bb82f86789aeab480a87337795ed49097f6728e
compatval1 <42851658+compatval1@users.noreply.github.com>
2018-08-30 15:31:56 -0700

    Manually cloning a repo
    Original content @ https://github.com/rafaelmaiolla/remote-vscode

---
+++ src/extension.ts
@@ -0,0 +1,107 @@
+'use strict';
+import * as vscode from 'vscode';
+import Server from './lib/Server';
+import Logger from './utils/Logger';
+import StatusBarItem from './lib/StatusBarItem';
+
+const L = Logger.getLogger('extension');
+
+var server : Server;
+var changeConfigurationDisposable : vscode.Disposable;
+var port : number;
+var host : string;
+var onStartup : boolean;
+var dontShowPortAlreadyInUseError : boolean;
+var statusBarItem : StatusBarItem;
+
+const startServer = () => {
+  L.trace('startServer');
+
+  if (!server) {
+    server = new Server();
+  }
+
+  if (!statusBarItem) {
+    statusBarItem = new StatusBarItem();
+  }
+
+  server.setPort(port);
+  server.setHost(host);
+  server.setDontShowPortAlreadyInUseError(dontShowPortAlreadyInUseError);
+  server.start(false);
+
+  statusBarItem.setServer(server);
+};
+
+const stopServer = () => {
+  L.trace('stopServer');
+
+  if (server) {
+    server.stop();
+  }
+};
+
+const initialize = () => {
+  L.trace('initialize');
+
+  var configuration = getConfiguration();
+  onStartup = configuration.onStartup;
+  port = configuration.port;
+  host = configuration.host;
+  dontShowPortAlreadyInUseError = configuration.dontShowPortAlreadyInUseError;
+
+  if (onStartup) {
+    startServer();
+  }
+};
+
+const getConfiguration = () => {
+  L.trace('getConfiguration');
+  var remoteConfig = vscode.workspace.getConfiguration('remote');
+
+  var configuration = {
+    onStartup: remoteConfig.get<boolean>('onstartup'),
+    dontShowPortAlreadyInUseError: remoteConfig.get<boolean>('dontShowPortAlreadyInUseError'),
+    port: remoteConfig.get<number>('port'),
+    host: remoteConfig.get<string>('host')
+  };
+
+  L.debug("getConfiguration", configuration);
+
+  return configuration;
+};
+
+const hasConfigurationChanged = (configuration) => {
+  L.trace('hasConfigurationChanged');
+  var hasChanged = ((configuration.port !== port) ||
+                    (configuration.onStartup !== onStartup) ||
+                    (configuration.host !== host) ||
+                    (configuration.dontShowPortAlreadyInUseError !== dontShowPortAlreadyInUseError));
+
+  L.debug("hasConfigurationChanged?", hasChanged);
+  return hasChanged;
+}
+
+const onConfigurationChange = () => {
+  L.trace('onConfigurationChange');
+
+  var configuration = getConfiguration();
+
+  if (hasConfigurationChanged(configuration)) {
+    initialize();
+  }
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  initialize();
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.startServer', startServer));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.stopServer', stopServer));
+
+  changeConfigurationDisposable = vscode.workspace.onDidChangeConfiguration(onConfigurationChange);
+}
+
+export function deactivate() {
+  stopServer();
+  changeConfigurationDisposable.dispose();
+}
